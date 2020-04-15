8c4568e14df90fd9d35eee56529d75d54a07cec0
Ricardo Alanis <ricardo.alanis@newports.com>
2020-04-14 23:46:35 -0500

    Override script

--- output_data/get_latest.py
+++ output_data/get_latest.py
@@ -9,7 +9,10 @@ def convert_back_to_str_file(date):
     return  'output_data/mexico-covid19-' + date.isoformat().split('T')[0] + '.csv'
 
 if __name__ == '__main__':
+    print('Finding newest file...')
     available_files = [file for file in os.listdir('output_data') if 'mexico' in file]
     max_date = max([get_date(file) for file in available_files])
     max_date_file = convert_back_to_str_file(max_date)
+    print('Generating latest.csv...')
     copyfile(max_date_file, 'output_data/latest.csv')
+    return True
